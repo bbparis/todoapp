4f21eac980bfa26e18fb8939c3b1b60b0427d39b
BB <bb2005@gmail.com>
2017-10-28 15:51:03 +0000

    Just a test

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,9 +1,10 @@
 class TodosController < ApplicationController
    
+   #here you have to define 'new' action and create an html file to it
    def new
        @todo =Todo.new
    end
-   
+   #here you have to create a todo
    def create
       @todo = Todo.create(todo_params)
       if @todo.save
@@ -25,3 +26,5 @@ class TodosController < ApplicationController
    end
    
 end
+
+XXXXXXXXXXXXXxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
