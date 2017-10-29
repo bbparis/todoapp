2337c031b0a8567d8245b3129b1e2c785d302a8f
BB <bb2005@gmail.com>
2017-10-29 06:44:13 +0000

    Remove redundancies and refactor code

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,11 +1,15 @@
 class TodosController < ApplicationController
    
+   #I'm going to autorize here set_todo for all the methods that need it
+   # and add it in the private section
+   before_action :set_todo, only: [:edit, :update, :show, :destroy]
+  # from here, I'm adding the top methods that I'm goint to use 
    def new
        @todo =Todo.new
    end
    
    def create
-      @todo = Todo.create(todo_params)
+      @todo =Todo.new(todo_params)
       if @todo.save
       flash[:notice] = "Todo was created successfully"
       redirect_to todo_path(@todo)
@@ -15,15 +19,12 @@ class TodosController < ApplicationController
    end    
    
    def show
-      @todo = Todo.find(params[:id])
    end
    
    def edit
-      @todo = Todo.find(params[:id])
    end
    
    def update
-      @todo =Todo.find(params[:id])
       if @todo.update(todo_params)
       flash[:notice] = "Todo was successfully updated"
       redirect_to todo_path(@todo)
@@ -37,13 +38,17 @@ class TodosController < ApplicationController
    end
    
    def destroy
-      @todo = Todo.find(params[:id])
       @todo.destroy
       flash[:notice]="Your Todo deleted successfuly"
       redirect_to todos_path
    end
    
+   #Here is the private part of todo
    private
+   
+   def set_todo
+      @todo = Todo.find(params[:id])
+   end
    
    def todo_params
       params.require(:todo).permit(:name, :description)
